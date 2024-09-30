a6ecc7c0495251d9e9cdfc59565d44cae00ee01f
Piterprot <pn.pietro134@gmail.com>
2024-09-30 13:40:17 +0200

    Loaded some file
    Loaded some file

---
+++ SintassiClassi/SintassiClassi/Libro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SintassiClassi
+{
+     class Libro
+    {
+
+        // Get = All'esterno la proprietà può essere letta 
+        // Set = All'esterno la proprietà può essere scritta
+        public string Titolo { get; set; }
+        public string Autore { get; set; }
+        public int Pagine { get; set; }
+        public decimal Prezzo { get; set; }
+        private int CodiceSegreto { get; }
+        
+        //costruttore => Il metodo per assegnare un valore alla proprietà 
+
+        public Libro() { 
+        // questo codice verrà eseguito quando si crea un oggetto con questa classe (new) 
+        CodiceSegreto = 321;
+        }
+        public decimal PrezzoInDollari()
+        {
+            decimal prezzoConvertito = Prezzo * 0.89M; 
+            return prezzoConvertito;
+
+        }
+    }
+}
---
+++ SintassiClassi/SintassiClassi/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using SintassiClassi;
+
+Console.WriteLine("Gestione Biblioteca");
+
+
+bool continua = true;
+Libro[] biblioteca = new Libro[500];
+int indice = 0;
+
+while (continua)
+{
+
+    // per creare un oggetto a partire da una classe: 
+    //Keyword: Nome della calsse 
+    // Nome dell'oggetto => Detto anche "Istanza"
+    // new => il programma alloca della memoria per l'oggetto creato
+    // "NomeDellaClasse()"
+    Libro oggettoLibro = new Libro();
+
+
+
+    Console.WriteLine("Inserisci il titolo");
+    oggettoLibro.Titolo = Console.ReadLine();
+
+    Console.WriteLine("Inserisci l'autore");
+    oggettoLibro.Autore = Console.ReadLine();
+
+    Console.WriteLine("Inserisci il numero di pagine");
+    oggettoLibro.Pagine = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Inserisci il prezzo");
+    oggettoLibro.Prezzo = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Vuoi inserire un altro libro? (s/n)");
+    string risposta = Console.ReadLine();
+    if (risposta == "n")
+    {
+        continua = false;
+    }
+    biblioteca[indice] = oggettoLibro;
+    indice++;
+
+    Console.WriteLine("Il prezzo in dollari sarà pari a " + oggettoLibro.PrezzoInDollari());
+
+
+}
+
+
+
